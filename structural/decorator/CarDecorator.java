6d3f8449c847e8d6a8f9c060ce75ee6ef3402ca8
YunusKaratepe <yunus.karatepe1699@gmail.com>
2022-02-01 01:08:27 +0300

    decorator pattern

---
+++ structural/decorator/CarDecorator.java
@@ -0,0 +1,15 @@
+package structural.decorator;
+
+public class CarDecorator implements Car {
+    
+    protected Car car;
+
+    public CarDecorator(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public void assemble() {
+        this.car.assemble();
+    }
+}
